8eacdc2eeef6263de2ab8ac4d4d424a46d35a1b7
Martin <lapetajemartin@gmail.com>
2022-05-25 03:26:04 +0800

    partial chat UI

--- chat_app/lib/src/screens/home/home_screen.dart
+++ chat_app/lib/src/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:chat_app/src/controllers/auth_controller.dart';
 import 'package:chat_app/src/controllers/chat_controller.dart';
 import 'package:chat_app/src/models/chat_user_model.dart';
@@ -61,7 +63,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text('Chatting from ${user?.username ?? '...'}'),
@@ -84,11 +85,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   builder: (context, Widget? w) {
                     return SingleChildScrollView(
                       controller: _scrollController,
-                      child: Column(
-                        children: [
-                          for (ChatMessage chat in _chatController.chats)
-                            ChatCard(chat: chat)
-                        ],
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            for (ChatMessage chat in _chatController.chats)
+                              ChatCard(chat: chat)
+                          ],
+                        ),
                       ),
                     );
                   }),
@@ -105,7 +109,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       focusNode: _messageFN,
                       controller: _messageController,
                       decoration: const InputDecoration(
-                        fillColor: Colors.white,
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(
                             Radius.circular(8),
@@ -147,58 +150,89 @@ class ChatCard extends StatelessWidget {
 
   final ChatMessage chat;
 
+  isYOU() {}
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(8),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: const Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Flexible(
-            child: Container(
-              color: Colors.white,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FutureBuilder<ChatUser>(
-                      future: ChatUser.fromUid(uid: chat.sentBy),
-                      builder: (context, AsyncSnapshot<ChatUser> snap) {
-                        if (snap.hasData) {
-                          return Text(chat.sentBy ==
-                                  FirebaseAuth.instance.currentUser?.uid
-                              ? 'You sent:'
-                              : '${snap.data?.username} sent');
-                        }
-                        return const Text('User');
-                      }),
-                  Text(chat.message),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [Text(chat.ts.toDate().toString())],
-                  )
-                  // Text(
-                  //     'Message seen by ${chat.seenBy}')
-                ],
-              ),
+
+        // width: 300,
+        // color: Colors.red,
+        alignment: chat.sentBy == FirebaseAuth.instance.currentUser?.uid
+            ? Alignment.bottomRight
+            : Alignment.centerLeft,
+        child:
+            // Container(
+            //   // width: chat.message.length * 20,
+            //   margin: const EdgeInsets.all(6),
+            //   padding: const EdgeInsets.all(8),
+            //   decoration: const BoxDecoration(
+            //     borderRadius: BorderRadius.all(Radius.circular(8)),
+            //     // color: Colors.black,
+            //   ),
+            //   child:
+            Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 3),
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 320),
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            child: Column(
+              // mainAxisSize: MainAxisSize.min,
+
+              children: [
+                Column(
+                  crossAxisAlignment:
+                      chat.sentBy == FirebaseAuth.instance.currentUser?.uid
+                          ? CrossAxisAlignment.end
+                          : CrossAxisAlignment.start,
+                  children: [
+                    FutureBuilder<ChatUser>(
+                        future: ChatUser.fromUid(uid: chat.sentBy),
+                        builder: (context, AsyncSnapshot<ChatUser> snap) {
+                          if (snap.hasData &&
+                              chat.sentBy !=
+                                  FirebaseAuth.instance.currentUser?.uid) {
+                            return Container(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Text(
+                                '${snap.data?.username}',
+                                style: Theme.of(context).textTheme.labelMedium,
+                              ),
+                            );
+                          }
+                          return const SizedBox();
+                        }),
+                    Container(
+                      margin: const EdgeInsets.all(2),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      // color: Colors.black,
+                      child: Text(
+                        chat.message,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Theme.of(context).colorScheme.onPrimary),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          chat.sentBy == FirebaseAuth.instance.currentUser?.uid
+                              ? EdgeInsets.only(right: 10)
+                              : EdgeInsets.only(left: 10),
+                      // child: Text(
+                      //   "data",
+                      //   style: Theme.of(context).textTheme.caption,
+                      // ),
+                    )
+                    // Text('Message seen by ${chat.seenBy}')
+                  ],
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-    );
+        ));
+    // );
   }
 }
